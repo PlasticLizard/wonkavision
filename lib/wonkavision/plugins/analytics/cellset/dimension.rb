d245c17f0ea7c266a4f6eb6f6a76b3ac988a4917
PlasticLizard <nathan_stults@hsihealth.com>
2011-02-15 16:15:25 -0800

    added #non_empty to dimension

--- lib/wonkavision/plugins/analytics/cellset/dimension.rb
+++ lib/wonkavision/plugins/analytics/cellset/dimension.rb
@@ -2,13 +2,19 @@ module Wonkavision
   module Analytics
     class CellSet
       class Dimension
-        attr_reader :definition,:members,:name
+        attr_reader :definition,:members,:name, :axis
         def initialize(axis,name,definition,members)
           @axis = axis
           @name = name.to_s
           @definition = definition
           @members = members ? members.values.map{ |mem_data| Member.new(self,mem_data)}.sort : []
         end
+
+        def non_empty(*parents)
+          key = parents << name
+          members.reject { |mem| axis[key].empty? }
+        end
+
       end
 
     end
--- lib/wonkavision/plugins/analytics/cellset/member.rb
+++ lib/wonkavision/plugins/analytics/cellset/member.rb
@@ -7,21 +7,27 @@ module Wonkavision
           @dimension = dimension
           @attributes = member_data
         end
+
         def caption
           attributes[dimension.definition.caption.to_s]
         end
+
         def key
           attributes[dimension.definition.key.to_s]
         end
+
         def sort
           attributes[dimension.definition.sort.to_s]
         end
+
         def <=>(other)
           sort <=> other.sort
         end
+
         def to_s
           key.to_s
         end
+
       end
 
     end
