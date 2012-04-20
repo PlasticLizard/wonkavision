6b401ed79ef0b031abb186d6d084f989af339e1e
Nathan <nathan_stults@hsihealth.com>
2012-04-19 23:16:42 -0700

    Added a toto

--- server/src/main/scala/dimensions/DimensionMember.scala
+++ server/src/main/scala/dimensions/DimensionMember.scala
@@ -4,7 +4,7 @@ import org.wonkavision.core.Dimension
 import org.wonkavision.core.Attribute
 
 case class DimensionMember(dimension : Dimension, attributeValues : Map[String,Any]) {
-
+	//TODO:Consider converting to target value up front to speed up query at expense of load time
 	def attributeValue(name : String) = {
 		val attr = dimension.getAttribute(name)
 		val rawVal = attributeValues(attr.name)
